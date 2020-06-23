2489b70fe7e0b4f8c7c44a8a723e342d465d607c
pete <pete.victoratos@aboutobjects.com>
2020-06-23 18:09:58 -0400

    got BIG SUR
    made a test file to see what's good

---
+++ BigSurTests/BigSurTests/BigSurTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigSurTestsApp.swift
+//  BigSurTests
+//
+//  Created by Peter Victoratos on 6/23/20.
+//
+
+import SwiftUI
+
+@main
+struct BigSurTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
